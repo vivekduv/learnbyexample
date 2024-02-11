0ab2acc3d218ad4251040771d8ce804bb49454bc
vivek <vivekduv@gmail.com>
2024-02-10 20:12:00 -0500

    ffff

--- camel/src/main/java/org/learnbyexample/Main.java
+++ camel/src/main/java/org/learnbyexample/Main.java
@@ -2,6 +2,6 @@ package org.learnbyexample;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
