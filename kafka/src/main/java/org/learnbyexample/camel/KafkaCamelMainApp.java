e6dd005eafd5e7cef103ccd1c5b2b25b68c678c9
vivek <vivekduv@gmail.com>
2024-11-02 21:58:29 -0400

    Test

--- kafka/src/main/java/org/learnbyexample/camel/KafkaCamelMainApp.java
+++ kafka/src/main/java/org/learnbyexample/camel/KafkaCamelMainApp.java
@@ -6,7 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class KafkaCamelMainApp {
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("appContextCamel.xml");
+        ApplicationContext applicationContext=
+                new ClassPathXmlApplicationContext("appContextCamel.xml");
         //main.configure().addRoutesBuilder(new AddRoutes());
         main.run(args);
     }
