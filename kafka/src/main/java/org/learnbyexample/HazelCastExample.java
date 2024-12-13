7a07b3d4faba2b8b152bc9021d6443a3ca539f16
vivek <vivekduv@gmail.com>
2024-12-12 23:18:47 -0500

    reader nscc

---
+++ kafka/src/main/java/org/learnbyexample/HazelCastExample.java
@@ -0,0 +1,26 @@
+package org.learnbyexample;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+public class HazelCastExample {
+    public static void main(String[] args) {
+        // Configure the Hazelcast Client
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress("10.0.0.77:5701"); // Replace with your cluster address
+
+        // Create a Hazelcast Client Instance
+        HazelcastInstance hazelcastClient = HazelcastClient.newHazelcastClient(clientConfig);
+
+        // Access a distributed map
+        IMap<String, String> map = hazelcastClient.getMap("my-distributed-map");
+
+        // Perform operations
+        //map.put("key", "valuexxxxx");
+        System.out.println("Value for 'key': " + map.get("key"));
+
+        // Shutdown the client
+        hazelcastClient.shutdown();
+    }
+}
