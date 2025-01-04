c1cbe3efebd86536eac339cf1ce55c3b5082e4bf
vivek <vivekduv@gmail.com>
2025-01-04 17:10:02 -0500

    cache clients

--- cache/src/main/java/org/learnbyexample/HazelCastExample.java
+++ cache/src/main/java/org/learnbyexample/HazelCastExample.java
@@ -1,4 +1,5 @@
 package org.learnbyexample;
+
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -6,21 +7,12 @@ import com.hazelcast.map.IMap;
 
 public class HazelCastExample {
     public static void main(String[] args) {
-        // Configure the Hazelcast Client
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress("localhost"); // Replace with your cluster address
-
-        // Create a Hazelcast Client Instance
         HazelcastInstance hazelcastClient = HazelcastClient.newHazelcastClient(clientConfig);
-
-        // Access a distributed map
-        IMap<String, String> map = hazelcastClient.getMap("my-distributed-map");
-
-        // Perform operations
-       // map.put("key", "valuexxxxx");
-        System.out.println("Value for 'key': " + map.get("key"));
-
-        // Shutdown the client
+        IMap<String, String> map = hazelcastClient.getMap("my-distributed-map1");
+        map.put("key1", "value1");
+        System.out.println("Value for 'key1': " + map.get("key1"));
         hazelcastClient.shutdown();
     }
 }
--- cache/src/main/java/org/learnbyexample/RedisClientExample.java
+++ cache/src/main/java/org/learnbyexample/RedisClientExample.java
@@ -4,30 +4,21 @@ import redis.clients.jedis.Jedis;
 
 public class RedisClientExample {
     public static void main(String[] args) {
-        // Replace "localhost" and "6379" with your Redis server's address and port
-        String redisHost = "10.0.0.77";
+        String redisHost = "localhost";
         int redisPort = 6379;
 
-        // Connecting to Redis server
         try (Jedis jedis = new Jedis(redisHost, redisPort)) {
             System.out.println("Connected to Redis");
-
-            // Set a key-value pair in Redis
-
-           jedis.set("name1", "Redis1");
+            jedis.set("name1", "Redis1");
             System.out.println("Set key 'name1' to value 'Redis1'");
 
-            // Retrieve the value of the key
-            String value = jedis.get("name");
-            System.out.println("Retrieved value for key 'name': " + value);
-
-            // Increment a counter
+            String value = jedis.get("name1");
+            System.out.println("Retrieved value for key 'name1': " + value);
             jedis.set("counter", "0");
             jedis.incr("counter");
             jedis.incr("counter");
             System.out.println("Counter value: " + jedis.get("counter"));
 
-            // Delete a key
             //jedis.del("name");
             //System.out.println("Deleted key 'name'");
         } catch (Exception e) {
