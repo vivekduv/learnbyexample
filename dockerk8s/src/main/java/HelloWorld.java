360b110bc48729a185c03744655ffbdafa1da5a3
vivek <vivekduv@gmail.com>
2025-01-10 19:23:07 -0500

    new modules

---
+++ dockerk8s/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world2!");
+
+        while (true) {
+            Thread.sleep(20000L);
+            System.out.println("sleeping..222");
+        }
+    }
+}
